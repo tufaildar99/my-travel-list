fe8e7e7073416fce85ad145cc350cb163be03e71
Tufail Dar <tufaildar2016@gmail.com>
2024-01-04 13:00:47 +0530

    Add form input and select options

--- src/App.js
+++ src/App.js
@@ -26,6 +26,15 @@ function Form() {
   return (
     <div className="add-form">
       <h3>What do you need for your trip ?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="item.."></input>
+      <button>Add</button>
     </div>
   );
 }
