632876d3ff965bc4e0419096891b05ce7fe10645
Tufail Dar <tufaildar2016@gmail.com>
2024-01-05 13:06:27 +0530

    Add delete functionality to PackingList component

--- src/App.js
+++ src/App.js
@@ -12,11 +12,15 @@ export default function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleDelete(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onhandleDelete={handleDelete} />
       <Stats />
     </div>
   );
@@ -69,25 +73,25 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onhandleDelete }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} />
+          <Item item={item} onhandleDelete={onhandleDelete} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onhandleDelete }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onhandleDelete(item.id)}>❌</button>
     </li>
   );
 }
